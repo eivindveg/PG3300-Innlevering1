90eb8bfacfbf9d85bf2ca7a121b7e9cebc209005
eivindveg <eivindveg@hotmail.com>
2014-09-28 14:27:14 +0200

    Reformat code

--- SnakeMess/SnakeMess.cs
+++ SnakeMess/SnakeMess.cs
@@ -1,162 +1,237 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
-
-// WARNING: DO NOT code like this. Please. EVER! 
-//          "Aaaargh!" 
-//          "My eyes bleed!" 
-//          "I facepalmed my facepalm." 
-//          Etc.
-//          I had a lot of fun obfuscating this code though! And I can now (proudly?) say that this is the uggliest short piece of code I've ever worked with! :-)
-//          (And yes, it could have been a lot ugglier! But the idea wasn't to make it fuggly-uggly, just funny-uggly, sweet-uggly, or whatever you want to call it.)
-//
-//          -Tomas
-//
-namespace SnakeMess
+﻿namespace SnakeMess
 {
-	class Coord
-	{
-		public const string Ok = "Ok";
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
 
-		public int X; public int Y;
-        public Coord( int x = 0, int y = 0 ) { X = x; Y = y; }
-        public Coord( Coord input ) { X = input.X; Y = input.Y; }
+    /// <summary>
+    ///     The coord.
+    /// </summary>
+    internal class Coord
+    {
+        /// <summary>
+        /// The ok.
+        /// </summary>
+        public const string Ok = "Ok";
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Coord"/> class.
+        /// </summary>
+        /// <param name="x">
+        /// The x.
+        /// </param>
+        /// <param name="y">
+        /// The y.
+        /// </param>
+        public Coord(int x = 0, int y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Coord"/> class.
+        /// </summary>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        public Coord(Coord input)
+        {
+            X = input.X;
+            Y = input.Y;
+        }
+
+        /// <summary>
+        /// Gets or sets the x.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the y.
+        /// </summary>
+        public int Y { get; set; }
     }
 
-    class SnakeMess
-	{
-		public static void Main( string[] arguments )
-		{
+    /// <summary>
+    ///     The snake mess.
+    /// </summary>
+    internal class SnakeMess
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="arguments">
+        /// The arguments.
+        /// </param>
+        public static void Main(string[] arguments)
+        {
             bool gg = false, pause = false, inUse = false;
-			short newDir = 2; // 0 = up, 1 = right, 2 = down, 3 = left
-			short last = newDir;
+            short newDir = 2; // 0 = up, 1 = right, 2 = down, 3 = left
+            short last = newDir;
             int boardW = Console.WindowWidth, boardH = Console.WindowHeight;
-            Random rng = new Random();
-            Coord app = new Coord();
-            List<Coord> snake = new List<Coord>();
-			snake.Add( new Coord( 10, 10 ) ); snake.Add( new Coord( 10, 10 ) ); snake.Add( new Coord( 10, 10 ) ); snake.Add( new Coord( 10, 10 ) );
-            Console.CursorVisible = false; 
-            Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition( 10, 10 ); Console.Write( "@" );
-			while ( true )
-			{
-				app.X = rng.Next( 0, boardW ); app.Y = rng.Next( 0, boardH );
-				bool spot = true;
-				foreach ( Coord i in snake )
-					if ( i.X == app.X && i.Y == app.Y )
-					{
-						spot = false;
-						break;
-					}
-				if ( spot )
-				{
-					Console.ForegroundColor = ConsoleColor.Red; Console.SetCursorPosition( app.X, app.Y ); Console.Write( "$" );
-					break;
-				}
-			}
-			Stopwatch t = new Stopwatch();
-			t.Start();
-			while ( !gg )
-			{
-				if ( Console.KeyAvailable )
-				{
-					ConsoleKeyInfo cki = Console.ReadKey( true );
-					if ( cki.Key == ConsoleKey.Escape )
-						gg = true;
-					else if ( cki.Key == ConsoleKey.Spacebar )
-						pause = !pause;
-					else if ( cki.Key == ConsoleKey.UpArrow && last != 2 )
-						newDir = 0;
-					else if ( cki.Key == ConsoleKey.RightArrow && last != 3 )
-						newDir = 1;
-					else if ( cki.Key == ConsoleKey.DownArrow && last != 0 )
-						newDir = 2;
-					else if ( cki.Key == ConsoleKey.LeftArrow && last != 1 )
-						newDir = 3;
-				}
-				if ( !pause )
-				{
-					if ( t.ElapsedMilliseconds < 100 )
-						continue;
-					t.Restart();
-					Coord tail = new Coord(snake.First());
-					Coord head = new Coord(snake.Last());
-					Coord newH = new Coord(head);
-					switch ( newDir )
-					{
-						case 0:
-							newH.Y -= 1;
-							break;
-						case 1:
-							newH.X += 1;
-							break;
-						case 2:
-							newH.Y += 1;
-							break;
-						case 3:
-						default:
-							newH.X -= 1;
-							break;
-					}
-					if ( newH.X < 0 || newH.X >= boardW )
-						gg = true;
-					else if ( newH.Y < 0 || newH.Y >= boardH )
-						gg = true;
-					if ( newH.X == app.X && newH.Y == app.Y )
-					{
-						if ( snake.Count + 1 >= boardW * boardH )
-							// No more room to place apples -- game over.
-							gg = true;
-						else
-						{
-							while ( true )
-							{
-								app.X = rng.Next( 0, boardW ); app.Y = rng.Next( 0, boardH );
-								bool found = true;
-								foreach ( Coord i in snake )
-									if ( i.X == app.X && i.Y == app.Y )
-									{
-										found = false;
-										break;
-									}
-								if ( found )
-								{
-									inUse = true;
-									break;
-								}
-							}
-						}
-					}
-					if ( !inUse )
-					{
-						snake.RemoveAt( 0 );
-						foreach ( Coord x in snake )
-							if ( x.X == newH.X && x.Y == newH.Y )
-							{
-								// Death by accidental self-cannibalism.
-								gg = true;
-								break;
-							}
-					}
-					if ( !gg )
-					{
-						Console.ForegroundColor = ConsoleColor.Green; 
-                        Console.SetCursorPosition( head.X, head.Y ); Console.Write( "O" );
-						if ( !inUse )
-						{
-							Console.SetCursorPosition( tail.X, tail.Y ); Console.Write( " " );
-						}
-						else
-						{
-							Console.ForegroundColor = ConsoleColor.Red; Console.SetCursorPosition( app.X, app.Y ); Console.Write( "$" );
-							inUse = false;
-						}
-						snake.Add( newH );
-						Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition( newH.X, newH.Y ); Console.Write( "@" );
-						last = newDir;
-					}
-				}
-			}
-		}
-	}
+            var rng = new Random();
+            var app = new Coord();
+            var snake = new List<Coord>();
+            snake.Add(new Coord(10, 10));
+            snake.Add(new Coord(10, 10));
+            snake.Add(new Coord(10, 10));
+            snake.Add(new Coord(10, 10));
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(10, 10);
+            Console.Write("@");
+            while (true)
+            {
+                app.X = rng.Next(0, boardW);
+                app.Y = rng.Next(0, boardH);
+                var spot = snake.All(i => i.X != app.X || i.Y != app.Y);
+
+                if (!spot)
+                {
+                    continue;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(app.X, app.Y);
+                Console.Write("$");
+                break;
+            }
+
+            var t = new Stopwatch();
+            t.Start();
+            while (!gg)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var cki = Console.ReadKey(true);
+                    switch (cki.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            gg = true;
+                            break;
+                        case ConsoleKey.Spacebar:
+                            pause = !pause;
+                            break;
+                        default:
+                            if (cki.Key == ConsoleKey.UpArrow && last != 2)
+                            {
+                                newDir = 0;
+                            }
+                            else if (cki.Key == ConsoleKey.RightArrow && last != 3)
+                            {
+                                newDir = 1;
+                            }
+                            else if (cki.Key == ConsoleKey.DownArrow && last != 0)
+                            {
+                                newDir = 2;
+                            }
+                            else if (cki.Key == ConsoleKey.LeftArrow && last != 1)
+                            {
+                                newDir = 3;
+                            }
+
+                            break;
+                    }
+                }
+
+                if (pause || t.ElapsedMilliseconds < 100)
+                {
+                    continue;
+                }
+
+                t.Restart();
+                var tail = new Coord(snake.First());
+                var head = new Coord(snake.Last());
+                var newH = new Coord(head);
+                switch (newDir)
+                {
+                    case 0:
+                        newH.Y -= 1;
+                        break;
+                    case 1:
+                        newH.X += 1;
+                        break;
+                    case 2:
+                        newH.Y += 1;
+                        break;
+                    default:
+                        newH.X -= 1;
+                        break;
+                }
+
+                if (newH.X < 0 || newH.X >= boardW)
+                {
+                    gg = true;
+                }
+                else if (newH.Y < 0 || newH.Y >= boardH)
+                {
+                    gg = true;
+                }
+
+                if (newH.X == app.X && newH.Y == app.Y)
+                {
+                    if (snake.Count + 1 >= boardW * boardH)
+                    {
+                        // No more room to place apples -- game over.
+                        gg = true;
+                    }
+                    else
+                    {
+                        while (true)
+                        {
+                            app.X = rng.Next(0, boardW);
+                            app.Y = rng.Next(0, boardH);
+                            var found = snake.All(i => i.X != app.X || i.Y != app.Y);
+
+                            if (!found)
+                            {
+                                continue;
+                            }
+
+                            inUse = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!inUse)
+                {
+                    snake.RemoveAt(0);
+                    if (snake.Any(x => x.X == newH.X && x.Y == newH.Y))
+                    {
+                        gg = true;
+                    }
+                }
+
+                if (gg)
+                {
+                    continue;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.SetCursorPosition(head.X, head.Y);
+                Console.Write("O");
+                if (!inUse)
+                {
+                    Console.SetCursorPosition(tail.X, tail.Y);
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.SetCursorPosition(app.X, app.Y);
+                    Console.Write("$");
+                    inUse = false;
+                }
+
+                snake.Add(newH);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.SetCursorPosition(newH.X, newH.Y);
+                Console.Write("@");
+                last = newDir;
+            }
+        }
+    }
 }
