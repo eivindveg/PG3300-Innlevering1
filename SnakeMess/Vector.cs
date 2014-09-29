db6b5d6fe1090483675d01ed2e792fb72a801c40
eivindveg <eivindveg@hotmail.com>
2014-09-29 15:25:27 +0200

    Build vector object

--- SnakeMess/Vector.cs
+++ SnakeMess/Vector.cs
@@ -23,5 +23,17 @@ public class Vector
 		set;
 	}
 
+    public Vector()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Vector(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
 }
 
