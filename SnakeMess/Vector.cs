4053a8586ffbfbe65bbd45d8baeb33ce07812dfa
eivindveg <eivindveg@hotmail.com>
2014-10-02 17:09:39 +0200

    Static method to get a new vector from behind a vector

--- SnakeMess/Vector.cs
+++ SnakeMess/Vector.cs
@@ -11,5 +11,22 @@
             X = x;
             Y = y;
         }
+
+        public static Vector DirectlyBehind(Direction fromDirection, Vector fromVector)
+        {
+            switch (fromDirection)
+            {
+                case Direction.Down:
+                    return new Vector(fromVector.X, fromVector.Y - 1);
+                case Direction.Up:
+                    return new Vector(fromVector.X, fromVector.Y + 1);
+                case Direction.Left:
+                    return new Vector(fromVector.X + 1, fromVector.Y);
+                case Direction.Right:
+                    return new Vector(fromVector.X - 1, fromVector.Y);
+            }
+
+            return fromVector;
+        }
     }
 }
